b9b4667b474747ae1d292cc98a74c2b17702bb60
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-03-30 22:40:49 +0530

    hero section file is created and h1 text is added with logo image

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import {marvelLogo} from '../assets'
+
+export const HeroSection = () => {
+  return (
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
+
+      <img 
+        src={marvelLogo} 
+        className='max-h-11 lg:h-11 mt-20'
+      />
+
+      <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>
+        Guardians <br /> of the Galaxy
+      </h1>
+
+    </div>
+  )
+}
