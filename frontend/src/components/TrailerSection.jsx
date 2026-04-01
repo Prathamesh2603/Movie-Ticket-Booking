c6b72728df84d35f44b755c45eaeed82cd5190fd
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-01 20:07:55 +0530

    Movie trailer sections is created and the first trailer is added

---
+++ frontend/src/components/TrailerSection.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { dummyTrailers } from "../assets/assets";
+import { BlurCircle } from "./BlurCircle"
+import ReactPlayer from "react-player";
+
+export const TrailerSection = () => {
+  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden">
+      
+      <p className="text-gray-300 font-medium text-lg max-w-240 mx-auto">
+        Trailers
+      </p>
+
+      {/* Watch Movie Trailers */}
+      <div className="relative mt-6">
+        <BlurCircle top="-100px" right="-100px" />
+        <ReactPlayer
+          url={currentTrailer.videoUrl} 
+          controls={false} 
+          className="mx-auto max-w-full"
+          width="960px"
+          height="540px"
+        />
+      </div>
+
+    </div>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { FeatureSection } from "../components/FeatureSection"
 import { HeroSection } from "../components/HeroSection"
+import { TrailerSection } from "../components/TrailerSection"
 
 export const Home = () => {
   return (
     <>
       <HeroSection />
       <FeatureSection />
+      <TrailerSection />
     </>
   )
 }
