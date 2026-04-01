42a991ea1cf4644f13c30a58bfbde0d332cea348
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-01 22:28:27 +0530

    minor changes

--- frontend/src/components/TrailerSection.jsx
+++ frontend/src/components/TrailerSection.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { dummyTrailers } from "../assets/assets";
 import { BlurCircle } from "./BlurCircle"
-import ReactPlayer from "react-player";
+import ReactPlayer from 'react-player'
+import { PlayCircleIcon } from "lucide-react";
 
 export const TrailerSection = () => {
   const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
@@ -16,13 +17,30 @@ export const TrailerSection = () => {
       {/* Watch Movie Trailers */}
       <div className="relative mt-6">
         <BlurCircle top="-100px" right="-100px" />
-        <ReactPlayer
-          url={currentTrailer.videoUrl} 
-          controls={false} 
-          className="mx-auto max-w-full"
-          width="960px"
-          height="540px"
-        />
+        <video controls autoPlay muted loop className="mx-auto max-w-full" width="960" height="540">
+          <source src={currentTrailer.videoUrl} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto">
+        {dummyTrailers.map((trailer) => (
+          <div 
+            key={trailer.image}
+            onClick={() => setCurrentTrailer(trailer)} 
+            className="relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 transition max-md:h-60 md:max-h-60 cursor-pointer"
+          >
+            <img 
+              src={trailer.image} 
+              alt="trailer"
+              className="rounded-lg w-full h-full object-cover brightness-75" 
+            />
+            <PlayCircleIcon 
+              strokeWidth={1.6}
+              className="absolute top-1/2 left-1/2 w-5 md:w-8 md:h-12 transform -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        ))}
       </div>
 
     </div>
