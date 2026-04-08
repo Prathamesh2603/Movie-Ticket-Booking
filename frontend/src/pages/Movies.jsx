29bee7d25bb784b396591c1681ee142363f9bb79
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-08 12:23:18 +0530

    blur circle is added to the movies page

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,9 +1,14 @@
 import { dummyShowsData } from "../assets/assets"
+import { BlurCircle } from "../components/BlurCircle"
 import { MovieCard } from "../components/MovieCard"
 
 export const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className="relative my-40 mb-60 px-6 mb:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+
+      <BlurCircle top="150px" left="0px" />
+      <BlurCircle bottom="50px" right="50px" />
+
       <h1 className="text-lg font-medium my-4">
         Now Showing
       </h1>
@@ -15,6 +20,7 @@ export const Movies = () => {
           />
         ))}
       </div>
+
     </div>
   ) : (
     <div>
