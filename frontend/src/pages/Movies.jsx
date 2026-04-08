685a9616dcd4fed280d98652a82a0a6d0e047819
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-08 12:20:03 +0530

    styling is given to the dummyshowdata if the length is greater than 0

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -3,14 +3,18 @@ import { MovieCard } from "../components/MovieCard"
 
 export const Movies = () => {
   return dummyShowsData.length > 0 ? (
-    <div>
-      <h1>Now Showing</h1>
-      {dummyShowsData.map(movie => (
-        <MovieCard 
-          movie={movie} 
-          key={movie._id} 
-        />
-      ))}
+    <div className="relative my-40 mb-60 px-6 mb:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      <h1 className="text-lg font-medium my-4">
+        Now Showing
+      </h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map(movie => (
+          <MovieCard 
+            movie={movie} 
+            key={movie._id} 
+          />
+        ))}
+      </div>
     </div>
   ) : (
     <div>
