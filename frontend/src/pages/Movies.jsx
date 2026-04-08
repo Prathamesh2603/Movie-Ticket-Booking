ed761d0f8433dee7702d1b8f6b3cd9ff579a49a3
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-08 12:15:03 +0530

    showing movie data in the form of movie card in movies page

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,5 +1,20 @@
+import { dummyShowsData } from "../assets/assets"
+import { MovieCard } from "../components/MovieCard"
+
 export const Movies = () => {
-  return (
-    <div>Movies</div>
+  return dummyShowsData.length > 0 ? (
+    <div>
+      <h1>Now Showing</h1>
+      {dummyShowsData.map(movie => (
+        <MovieCard 
+          movie={movie} 
+          key={movie._id} 
+        />
+      ))}
+    </div>
+  ) : (
+    <div>
+
+    </div>
   )
 }
