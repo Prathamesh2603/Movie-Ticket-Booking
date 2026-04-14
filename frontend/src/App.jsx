63779834e5f15c4914536645a0b362c5bc3fbb02
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-14 22:08:02 +0530

    found the mistake

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
-        <Route path="/movies/:id/date" element={<SeatLayout />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBooking />} />
         <Route path="/favorite" element={<Favourite />} />
       </Routes>
