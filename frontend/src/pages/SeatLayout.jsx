fb458d70a78ad4e191e5ccdbd16879dc50d08baf
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-04-20 11:39:23 +0530

    seat layout for the seatlayout page is done

--- frontend/src/pages/SeatLayout.jsx
+++ frontend/src/pages/SeatLayout.jsx
@@ -13,6 +13,7 @@ export const SeatLayout = () => {
   const [selectedTime, setSelectedTime] = useState(null);
   const [show, setShow] = useState(null);
   const navigate = useNavigate();
+  const groupRows = [["A", "B"],["C", "D"],["E", "F"],["G", "H"],["I", "J"]];
 
   const getShow = async () => {
     const show = dummyShowsData.find(show => show._id === id);
@@ -36,7 +37,7 @@ export const SeatLayout = () => {
   }
 
   // Select Seats Function 
-  const renderSeats = (row, count = 9) => {
+  const renderSeats = (row, count = 9) => (
     <div key={row} className="flex gap-2 mt-2">
       <div className="flex flex-wrap items-center justify-center gap-2">
         {Array.from({ length: count }, (_, i) => {
@@ -54,7 +55,7 @@ export const SeatLayout = () => {
       }
       </div>
     </div>
-  }
+  )
 
   useEffect(() => {
     getShow();
@@ -92,6 +93,18 @@ export const SeatLayout = () => {
         <p className="text-gray-400 text-sm mb-6">
           SCREEN SIDE
         </p>
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-300">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+            {groupRows[0].map(row => renderSeats(row))}
+          </div>
+          <div className="grid grid-cols-2 gap-11">
+            {groupRows.slice(1).map((group, idx) => (
+              <div key={idx}>
+                {group.map(row => renderSeats(row))}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
     </div>
