2e6067e88d8838067c953162d0fd26c08cdaadda
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-05-26 13:27:08 +0530

    mapping the booking useState to display the movies in card format

--- frontend/src/pages/MyBooking.jsx
+++ frontend/src/pages/MyBooking.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { dummyBookingData } from "../assets/assets";
 import { Loading } from "../components/Loading";
 import { BlurCircle } from "../components/BlurCircle";
+import { timeFormat } from "../lib/timeFormat";
 
 export const MyBooking = () => {
   const [bookings, setBookings] = useState([]); // Booking is empty for new user
@@ -36,6 +37,35 @@ export const MyBooking = () => {
       <h1 className="text-lg font-semibold mb-4">
         My Bookings
       </h1>
+
+      {bookings.map((item, idx) => (
+        <div
+          key={idx} 
+          className="flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl"
+        >
+          <div className="flex flex-col md-flex-row">
+            <img 
+              src={item.show.movie.poster_path} 
+              alt="" 
+              className="md:max-w-45 aspect-video h-auto object-cover object-bottom rounded"
+            />
+
+            <div className="flex flex-col p-4">
+              <p className="text-lg font-semibold">
+                {item.show.movie.title}
+              </p>
+              <p className="text-gray-400 text-sm">
+                {timeFormat(item.show.movie.runtime)}
+              </p>
+              <p className="text-gray-400 text-sm mt-auto">
+                {item.show.showDateTime}
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+      ))}
 
     </div>
   ) : <Loading />
