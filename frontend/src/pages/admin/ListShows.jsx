0e4dcf2a888ee7c2985d670fd3e059a8c761737b
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-06-18 00:20:49 +0530

    title and table head for the list show is done

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { dummyShowsData } from "../../assets/assets";
+import { Loading } from "../../components/Loading";
+import { Title } from "../../components/admin/Title";
 
 export const ListShows = () => {
   const [shows, setShows] = useState([]);
@@ -29,7 +31,27 @@ export const ListShows = () => {
     getAllShows();
   }, []);
 
-  return (
-    <div>ListShows</div>
-  )
+  return !loading 
+    ? (
+        <>
+
+          <Title text1="List" text2="Shows" />
+          <div className="max-w-4xl mt-6 overflow-x-auto">
+
+            <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
+              <thead>
+                <tr className="bg-primary/20 text-left text-white">
+                  <th className="p-2 font-medium pl-5">Movie Name</th>
+                  <th className="p-2 font-medium">Show Time</th>
+                  <th className="p-2 font-medium">Total Bookings</th>
+                  <th className="p-2 font-medium">Earnings</th>
+                </tr>
+              </thead>
+            </table>
+
+          </div>
+
+        </>
+      )
+    : <Loading />
 }
