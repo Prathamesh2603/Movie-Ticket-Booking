2d84122d58e61bf98c2553b7f3d8ad724d888ea2
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-06-18 14:54:14 +0530

    state and lifecycle for the booking list is set

--- frontend/src/pages/admin/ListBookings.jsx
+++ frontend/src/pages/admin/ListBookings.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Loading } from '../../components/Loading';
+import { dummyBookingData } from '../../assets/assets';
+import { Title } from '../../components/admin/Title';
 
 export const ListBookings = () => {
-  return (
-    <div>ListBookings</div>
-  )
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const getAllBookings = async () => {
+    setDashboardData(dummyBookingData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAllBookings();
+  }, []);
+
+  return !loading
+    ? (
+        <>
+          
+        </>
+      )
+    : <Loading />
 }
